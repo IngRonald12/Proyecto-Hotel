cb3328a31418a826ebd7a2f07b9c5bc0ecdd56a1
IngRonald12 <129900670+IngRonald12@users.noreply.github.com>
2023-04-18 22:03:03 -0500

    Update Reserva de hotel.py

--- Reserva de hotel.py
+++ Reserva de hotel.py
@@ -6,41 +6,73 @@ habitaciones = [
     {"numero": 102, "disponible": True, "precio": 60, },
     {"numero": 103, "disponible": True, "precio": 70, },
     {"numero": 104, "disponible": True, "precio": 80, },
-    {"numero": 105, "disponible": True, "precio": 90,}
+    {"numero": 105, "disponible": True, "precio": 90, }
 ]
 
+Registro_personas={1003259544:"Ronald", 2515:"Jorman"} 
+
+def eliminar():
+    del Registro_personas["Documento"]
+
+def registro():
+    print("Bienvenido al registro Hotel bella vista.")
+    keys =int(input("Ingrese documento de identidad -> "))
+    value=input("Ingrese nombre - >")
+    Registro_personas[keys]=value
 
 
+def editar():
+    nombre_edit=input("ingrese el dato que quiere editar -> ")
+    Registro_personas.update({"Documento": nombre_edit})
 
-# Función para mostrar las habitaciones disponibles
+def login():
+    usuario = int(input("Ingrese su documento de identidad ->"))
+    if Registro_personas[usuario]:
+        print("BIENVENIDO")
+        print("---------------------------");print(Registro_personas[usuario]), print("---------------------------")
+
+#Función para mostrar las habitaciones disponibles
 def mostrar_habitaciones():
     for habitacion in habitaciones:
         if habitacion["disponible"]:
             print(f'Habitación {habitacion["numero"]} - precio por noche: {habitacion["precio"]}')
 # Función para hacer una reserva
-def hacer_reserva(numero_habitacion, noches):
+def hacer_reserva(numero_habitacion, noches, personas):
     for habitacion in habitaciones:
         if habitacion["numero"] == numero_habitacion and habitacion["disponible"]:
             habitacion["disponible"] = False
             total = habitacion["precio"] * noches
-            print(f'Reservaste la habitación {numero_habitacion} por {noches} noches. Total a pagar: {total}')
+            print("---------------------------------------------------------------------------------------")
+            print(f'Reservaste la habitación {numero_habitacion} para {personas} personas por {noches} noches. Total a pagar: {total}')
+            print("---------------------------------------------------------------------------------------")
             return
     print(f'Lo siento, la habitación {numero_habitacion} no está disponible.')
 
-# Mostrar registro de datos 
 
-
-# Mostrar las habitaciones disponibles
-print("Habitaciones disponibles:")
-mostrar_habitaciones()
-
-
-# Hacer una reserva
-numero_habitacion = int(input("Ingresa el número de la habitación que deseas reservar: "))
-noches = int(input("Ingresa el número de noches que te quedarás: "))
-hacer_reserva(numero_habitacion, noches)
-
-# Mostrar las habitaciones disponibles después de hacer la reserva
-print("Habitaciones disponibles:")
-mostrar_habitaciones()
-
+while True:
+        print("Menú:")
+        print("1. Agregar elemento")
+        print("2. Editar elemento")
+        print("3. Eliminar elemento")
+        print("4. hacer una reserva ")
+        print("5. Salir")
+        opcion = input("Seleccione una opción: ")
+    
+        if opcion == "1":
+            registro()
+        if opcion=="2":
+            editar()
+        if opcion=="3":
+            eliminar()
+        if opcion== "4":
+            Nombre=input("ingrese su nombre -> ")
+            documento=int(input("Ingrese su documento de identidad -> "))
+            mostrar_habitaciones()
+            numero_habitacion = int(input("Ingresa el número de la habitación que deseas reservar: "))
+            noches = int(input("Ingresa el número de noches que te quedarás: "))
+            personas=int(input("Ingrese la cantidad de personas ->"))
+            hacer_reserva(numero_habitacion, noches, personas)
+            break
+        if opcion=="5":
+            print("Cerrando programa...")
+            break
