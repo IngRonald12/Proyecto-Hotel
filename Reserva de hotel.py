56060233be21a2621f3691180827690554a30626
JhonMa05 <jmariolozano@unicesar.edu.co>
2023-04-22 23:28:00 -0500

    Update Reserva de hotel.py
    1_review
    Organice las funciones para mejor verlo en el orden CRUD

--- Reserva de hotel.py
+++ Reserva de hotel.py
@@ -2,22 +2,30 @@ from os import system
 system ("cls")
 # Definir una lista de habitaciones con su disponibilidad y precio por noche
 habitaciones = [
-    {"numero": 101, "disponible": True, "precio": 50, }, #Faltan datos para gestión
+    {"numero": 101, "disponible": True, "precio": 50, },
     {"numero": 102, "disponible": True, "precio": 60, },
     {"numero": 103, "disponible": True, "precio": 70, },
     {"numero": 104, "disponible": True, "precio": 80, },
     {"numero": 105, "disponible": True, "precio": 90, }
 ]
+## modificar para guardar datos
+Registro_personas={1003259544:"Ronald", 2515:"Jorman"} 
 
-#Gestión de reservas
+#CRUD 
+def registro():
+    print("Bienvenido al registro Hotel bella vista.")
+    Documento1=int(input("Ingrese documento de identidad -> "))
+    value=input("Ingrese nombre - >").lower()
+    Registro_personas.update({"Documento1":"value"});system("cls")
+    print(f"Señor {value} usted ha sido agregado exitosamente! ")
 
-#Gestion las habitaciones ocupadas 
-
-#datos = {101: [cedula, nombre, apellido, fechallegada, fechasalida]}
-
-Registro_personas={1003259544:"Ronald", 2515:"Jorman"} #aumentar esos datos
-
-datos_admin = {1065875411:["Didier guerrero", "dfguerrero", 909090]}
+def editar():
+    nombre_edit=input("ingrese el dato que quiere editar -> ")
+    if nombre_edit in Registro_personas:
+        Registro_personas.update({"Documento": nombre_edit})    
+    for nombre_edit in Registro_personas:
+        print(Registro_personas)
+        
 
 def eliminar():
     Delete=(input("Ingrese Dato que desea eliminar -> "))
@@ -27,33 +35,17 @@ def eliminar():
     elif Delete in Registro_personas:
         print(f"{Delete} No existe ")
 
-def registro():
-    print("Bienvenido al registro Hotel bella vista.")
-    Documento1=int(input("Ingrese documento de identidad -> "))
-    value=input("Ingrese nombre - >").lower()
-    #direccion=input("Ingrese su dirección ->").lower()
-    #telefono=int(input("ingrese su numero de telefono -> "))
-    Registro_personas.update({Documento1:value});system("cls")
-    print(f"Señor {value} usted ha sido agregado exitosamente! ")
-
-def editar():
-    nombre_edit=input("ingrese el dato que quiere editar -> ")
-    if nombre_edit in Registro_personas:
-        Registro_personas.update({"Documento": nombre_edit})    
-    for nombre_edit in Registro_personas:
-        print(Registro_personas)
-
-def login(): #usuario administrador
-    while True:
-        inicio=(input("¿Está usted registrado si/no? -> ")).lower();system("cls")
-        if inicio=="si":
-            usuario = int(input("Ingrese su documento de identidad ->"))
-            if usuario==datos_admin:
-                print("BIENVENIDO")
-            print("---------------------------");print(datos_admin[usuario]), print("---------------------------")
-        if inicio== "no":
-            print("Usted no es administrador ")
-        break
+         
+def login():
+    inicio=(input("¿Está usted registrado si/no? -> ")).lower();system("cls")
+    if inicio=="si":
+        usuario = int(input("Ingrese su documento de identidad ->"))
+        if usuario==Registro_personas:
+            print("BIENVENIDO")
+        print("---------------------------");print(Registro_personas[usuario]), print("---------------------------")
+    if inicio== "no":
+        registro()
+#--        
 
 #Función para mostrar las habitaciones disponibles
 def mostrar_habitaciones():
@@ -75,34 +67,37 @@ def hacer_reserva(numero_habitacion, noches, personas):
             return
     print(f'Lo siento, la habitación {numero_habitacion} no está disponible.')
 
+
+#en que momento se utiliza?
 login()
 
+#
 while True:
         print("Menú:")
-        print("1. Agregar elemento") #? especificar que?
+        print("1. Agregar elemento")#revisar metodo 'registro' U
         print("2. Editar elemento")
         print("3. Eliminar elemento")
-        print("4. Realizar una reserva ")
+        print("4. Hacer una reserva ")#revisar ''
         print("5. Salir")
         opcion = input("Seleccione una opción: ")
-        system ("cls")
-        while True:
-            if opcion == "1":
-                registro()
-            if opcion=="2":
-                editar()
-            if opcion=="3":
-                eliminar()
-            if opcion== "4":
-                Nombre=input("ingrese su nombre -> ")
-                documento=int(input("Ingrese su documento de identidad -> "))
-                mostrar_habitaciones()
-                numero_habitacion = int(input("Ingresa el número de la habitación que deseas reservar: "))
-                noches = int(input("Ingresa el número de noches que te quedarás: "))
-                personas=int(input("Ingrese la cantidad de personas ->"));system("cls")
-                hacer_reserva(numero_habitacion, noches, personas)
-                break
-            if opcion=="5":
-                print("Cerrando programa...")
-                break
-        break
+    
+        if opcion == "1":
+            registro()
+        if opcion=="2":
+            editar()
+        if opcion=="3":
+            eliminar()
+        if opcion== "4":
+
+            Nombre=input("ingrese su nombre -> ")
+            documento=int(input("Ingrese su documento de identidad -> "))
+            mostrar_habitaciones()
+            numero_habitacion = int(input("Ingresa el número de la habitación que deseas reservar: "))
+            noches = int(input("Ingresa el número de noches que te quedarás: "))
+            personas=int(input("Ingrese la cantidad de personas ->"));system("cls")
+            hacer_reserva(numero_habitacion, noches, personas)
+            break
+
+        if opcion=="5":
+            print("Cerrando programa...")
+            break
